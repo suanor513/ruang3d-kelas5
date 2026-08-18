982e43f3c9a76975090232ca60ad6c57ec63226f
suanor513 <suanor513@gmail.com>
2026-08-18 17:24:32 +0700

    Menambahkan fungsi dasar animasi jaring-jaring Kubus

--- script.js
+++ script.js
@@ -642,7 +642,26 @@ panelJaringKubus.push(sisi);
     scrollKeTampilan();
 
 }
-    
+
+// ======================================
+// ANIMASI JARING-JARING KUBUS
+// ======================================
+
+function animasiJaringKubus(){
+
+    if(!panelJaringKubus.length){
+        return;
+    }
+
+    panelJaringKubus.forEach(function(sisi){
+
+        sisi.rotation.z =
+        sisi.userData.sudutAwal;
+
+    });
+
+}
+
 // ======================================
 // SCROLL OTOMATIS KE BANGUN RUANG
 // ======================================
