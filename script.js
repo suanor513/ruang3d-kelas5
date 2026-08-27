abc10e963029f17828f74a19922994ab12890346
suanor513 <suanor513@gmail.com>
2026-08-27 15:04:22 +0700

    Menambahkan fungsi engsel Prisma Segitiga 3D

--- script.js
+++ script.js
@@ -1549,6 +1549,65 @@ function buatSegitigaPrisma(
     return segitiga;
 
 }
+
+    // ==================================
+    // FUNGSI ENGSEL PRISMA SEGITIGA
+    // ==================================
+
+    function buatEngselPrisma3D(
+        pivotX,
+        pivotY,
+        sisiX,
+        sisiY,
+        ukuranX,
+        ukuranY,
+        warna,
+        sumbu,
+        sudutTutup,
+        parent
+    ){
+
+        let pivot =
+            new THREE.Group();
+
+        pivot.position.set(
+            pivotX,
+            pivotY,
+            0
+        );
+
+        parent.add(pivot);
+
+        let sisi =
+            buatPanelPrisma(
+                ukuranX,
+                ukuranY,
+                warna
+            );
+
+        sisi.position.set(
+            sisiX,
+            sisiY,
+            0
+        );
+
+        pivot.add(sisi);
+
+        dataJaringPrisma3D.push({
+
+            pivot:pivot,
+
+            sumbu:sumbu,
+
+            sudutBuka:0,
+
+            sudutTutup:sudutTutup
+
+        });
+
+        return pivot;
+
+    }
     
 // ==================================
 // 3 SISI PERSEGI PANJANG
